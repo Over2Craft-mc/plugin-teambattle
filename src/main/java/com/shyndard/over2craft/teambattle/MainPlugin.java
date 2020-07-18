d7dac2d2ac18544324c745e25955a662ed6cae7f
Custo <shyndard@gmail.com>
2020-07-19 00:59:31 +0200

    Init project

---
+++ src/main/java/com/shyndard/over2craft/teambattle/MainPlugin.java
@@ -0,0 +1,17 @@
+package com.shyndard.over2craft.teambattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+	
+}
